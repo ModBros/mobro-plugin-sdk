4e52b6cd7fe65b240c5012a2829eeb251dc2e1e5
Manfred Graf <manfred.graf91@gmail.com>
2022-11-17 00:23:19 +0100

    refactor(plugin): adjusted methods of the MoBroPlugin interface

--- IMoBroPlugin.cs
+++ IMoBroPlugin.cs
@@ -13,26 +13,38 @@ public interface IMoBroPlugin : IDisposable
   /// <summary>
   /// Called once by the service upon initialization of the plugin.<br/>
   /// The passed instances of <see cref="IPluginSettings"/> and <see cref="IMoBroService"/> may be stored and used throughout
-  /// the whole lifetime of the plugin to register <see cref="IMoBroItem"/>s, push updated <see cref="IMetricValue"/>s, etc.
+  /// the whole lifetime of the plugin to register <see cref="IMoBroItem"/>s, push updated <see cref="MetricValue"/>s, etc.
   /// at any time.
   /// </summary>
+  /// <remarks>Only one of either <see cref="Init"/> or <see cref="InitAsync"/> needs to be implemented.</remarks>
   /// <param name="settings">The current plugin settings.</param>
   /// <param name="service">The <see cref="IMoBroService"/> implementation.</param>
-  /// <returns></returns>
-  public Task Init(IPluginSettings settings, IMoBroService service);
+  public void Init(IPluginSettings settings, IMoBroService service)
+  {
+  }
+
+  /// <summary>
+  /// Same as <see cref="Init"/>, just async.
+  /// </summary>
+  /// <remarks>Only one of either <see cref="Init"/> or <see cref="InitAsync"/> needs to be implemented.</remarks>
+  /// <param name="settings">The current plugin settings.</param>
+  /// <param name="service">The <see cref="IMoBroService"/> implementation.</param>
+  public Task InitAsync(IPluginSettings settings, IMoBroService service) => Task.CompletedTask;
 
   /// <summary>
   /// Called by to signal the plugin that it should pause monitoring and stop sending metric value updates, etc.<br/>
   /// This may be called due to the service switching into idle mode as no client has requested data for a prolonged time.
   /// </summary>
   /// <remarks>Any data sent while the plugin is 'paused' may be ignored by the service.</remarks>
-  /// <returns></returns>
-  public Task Pause();
+  public void Pause()
+  {
+  }
 
   /// <summary>
   /// Called to signal the plugin to resume monitoring and again start sending metric value updates, etc.<br/>
   /// Will only be called if the plugin has been paused by a call to <see cref="Pause"/>.
   /// </summary>
-  /// <returns></returns>
-  public Task Resume();
+  public void Resume()
+  {
+  }
 }
